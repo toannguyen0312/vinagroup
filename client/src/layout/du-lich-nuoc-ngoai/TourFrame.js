dd688a2018c6d113b6485fed3de0df28d34efb2a
Toan <tony.nguyen.1105@gmail.com>
2026-04-13 17:27:36 +0700

    new changes

---
+++ client/src/layout/du-lich-nuoc-ngoai/TourFrame.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import NewDashBoardNav from "../dashboard/NewDashBoardNav";
+import logovinagroup from "../dashboard/logovinagroup.jpg";
+
+
+function TourFrame() {
+    return (
+        <>
+            <div>
+                <p>028 3526 41668 | 0934 043 188</p>
+            </div>
+
+            <div className="logonav">
+                <Link to="/" className="navbar-brand">
+                <img src={logovinagroup} alt="VinaGroup Logo" className="logo-image" />
+                </Link>
+                <NewDashBoardNav toggleNav={toggleNav} mobileMenuOpen={mobileMenuOpen} />
+            </div>
+        </>
+    )
+}
--- client/src/layout/du-lich-nuoc-ngoai/TourNuocNgoai.js
+++ client/src/layout/du-lich-nuoc-ngoai/TourNuocNgoai.js
@@ -14,7 +14,9 @@ const [loading, setLoading] = useState(true);
 
 
     return (
-
+        <>
+            
+        </>
     );
 }
 
