b0f9be728b7e47ce168b394eeb9a46b0ce4d18d6
toannguyen <tony.nguyen.1105@gmail.com>
2026-05-31 21:14:17 -0700

    new changes on backend

--- back-end/src/db/seeds/00-tours.js
+++
@@ -1,7 +0,0 @@
-const toursData = require("./00-tours.json");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE tours RESTART IDENTITY CASCADE")
-    .then(() => knex("tours").insert(toursData));
-};
